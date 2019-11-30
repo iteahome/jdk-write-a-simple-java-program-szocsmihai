3e3fa49e1744bb1ea582f7abcaa30a4ab3bbdd8b
szocsmihai <szocsmihai@gmail.com>
2019-11-30 18:41:58 +0200

    Commit to test push in repo.

--- src/ro/iteahome/misusgreeting/MisusGreeting.java
+++ src/ro/iteahome/misusgreeting/MisusGreeting.java
@@ -2,6 +2,6 @@ package ro.iteahome.misusgreeting;
 
 public class MisusGreeting {
     public static void main(String[] args) {
-        System.out.println("Happy Holidays! Yours truly, Misu.");
+        System.out.println("Happy Holidays! Yours truly, Misu. Plus a push test.");
     }
 }
