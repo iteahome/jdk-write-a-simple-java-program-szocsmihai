d946980df47189c003b62a7c2382de1f61702766
szocsmihai <szocsmihai@gmail.com>
2019-11-30 21:12:55 +0200

    Playing around with repo to understand the mechanism.

--- src/ro/iteahome/misusgreeting/MisusGreeting.java
+++
@@ -1,7 +0,0 @@
-package ro.iteahome.misusgreeting;
-
-public class MisusGreeting {
-    public static void main(String[] args) {
-        System.out.println("Happy Holidays! Yours truly, Misu. Plus a push test.");
-    }
-}
