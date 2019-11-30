40cc4977fce7e2a812052d7b9b0be9085cc76393
szocsmihai <szocsmihai@gmail.com>
2019-11-30 15:12:45 +0200

    Initial commit: my first Git commit via IntelliJ Idea. A simple greeting program.

---
+++ src/ro/iteahome/misusgreeting/MisusGreeting.java
@@ -0,0 +1,7 @@
+package ro.iteahome.misusgreeting;
+
+public class MisusGreeting {
+    public static void main(String[] args) {
+        System.out.println("Happy Holidays! Yours truly, Misu.");
+    }
+}
